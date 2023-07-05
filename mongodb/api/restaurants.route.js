780c6f82562a8c4c75d9466d60dcfba06cb2fa64
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-07-05 22:42:34 +0530

    Add an api end point to add new restaurant details to the database

--- mongodb/api/restaurants.route.js
+++ mongodb/api/restaurants.route.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const getRestaurants = require("./routes/getRestaurants");
 const getRestaurantsById = require("./routes/getRestaurantsById");
+const addRestaurants = require("./routes/addRestaurants");
 
 const router = express.Router();
 
 router.route("/").get(getRestaurants);
 router.route("/id/:id").get(getRestaurantsById);
+router.route("/add").get(addRestaurants);
 
 module.exports = router; 
